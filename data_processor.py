ed8ccefa7a56fa02abf03c830b6267b8846249f0
krishna0712 <krishnaacharya072007@gmail.com>
2026-02-27 22:18:17 +0530

    this concludes the cleaning

---
+++ data_processor.py
@@ -0,0 +1,8 @@
+#here the data will be filtered, cleaned and the feature engineering.
+# pip install pandas numpy streamlit plotly openpyxl
+import pandas as pd 
+import numpy as np
+
+df = pd.read_csv("../Data_clean/combined_sale.csv")
+
+
